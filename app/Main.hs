d2d060e3633db910256948440c28999668ed0bb9
Eric Conlon <ejconlon@gmail.com>
2020-05-08 10:36:47 -0700

    init

---
+++ app/Main.hs
@@ -0,0 +1,7 @@
+module Main where
+
+import qualified Refextras.Main
+import Prelude
+
+main :: IO ()
+main = Refextras.Main.main
---
+++ src/Refextras/Main.hs
@@ -0,0 +1,6 @@
+module Refextras.Main where
+
+import Prelude
+
+main :: IO ()
+main = putStrLn "hello, world"
